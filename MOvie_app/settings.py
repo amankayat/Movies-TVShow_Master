0bb188589f616e0de53faddad16fbc2752077e63
amankayat <72142181+amankayat@users.noreply.github.com>
2023-01-05 18:29:43 +0530

    update

--- MOvie_app/settings.py
+++ MOvie_app/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'MOvie_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
