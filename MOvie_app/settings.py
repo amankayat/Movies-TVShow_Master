61aa75c5730bf4fdb6c37acb696f98ccd3d23d70
amankayat <72142181+amankayat@users.noreply.github.com>
2023-01-05 17:50:42 +0530

    vercel setting done

--- MOvie_app/settings.py
+++ MOvie_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u^f+c-j$=1my9cfkeea93dulzt@qd24)hp2@39=42&k9!pz$bl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
